9e925e3a50c24830694fd4354b876f13eadef1d2
Laurent Jouanneau <dev@ljouanneau.com>
2020-04-28 10:54:55 +0200

    New scripts to ease push/pull to/from transifex

---
+++ update_from_transifex.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+LOCALES_DIR="$(dirname $0)"
+
+export LOCALES_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+export AVAILABLE_LOCALES="es fi fr it pt ru"
+
+usage()
+{
+    echo "$0 [options] [locale]"
+    echo ""
+    echo "This script pull translations from transifex. It pull translations"
+    echo "of all languages or only the one given in parameter."
+    echo ""
+    echo "Options:"
+    echo "   -f    force to update translations, regardless of whether timestamps"
+    echo "         on the local computer are newer than those on the server"
+    echo "   "
+
+}
+
+FORCE=""
+ALL_LOCALES=""
+
+for i in $*
+do
+case $i in
+    -h|--help)
+    usage
+    exit 0
+    ;;
+    -f|--force)
+    FORCE="-f"
+    ;;
+    -*)
+      echo "ERROR: Unknown option: $i"
+      echo ""
+      usage
+      exit 1
+    ;;
+    *)
+    if [ "$ALL_LOCALES" = "" ]; then
+        ALL_LOCALES="$i"
+    else
+        echo "ERROR: Two many arguments"
+        usage
+        exit 3
+    fi
+    ;;
+esac
+done
+
+
+
+
+if [ "$ALL_LOCALES" == "" ]; then
+  ALL_LOCALES=$(echo "$AVAILABLE_LOCALES" | sed -r 's/ /,/g')
+fi
+
+echo $ALL_LOCALES
+
+tx pull $FORCE --mode developer -l "$ALL_LOCALES" --branch $LOCALES_BRANCH
+
+
