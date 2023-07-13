e71b71411d572ddfac5f95a351896d39d99b7625
Etienne Trimaille <gustrimaille@yahoo.fr>
2023-07-13 15:10:37 +0200

    Display a warning when visiting old version of the documentation, ask robots to not index if necessary

--- source/conf.py
+++ source/conf.py
@@ -132,6 +132,9 @@ except Error:
 
 html_context = {
         'theme_logo_only': True,
+        # When a LWC version reaches the end of life, set this to True to show an information
+        # message on the top of the page.
+        'outdated': False,
         'lizmap_versions': lizmap_versions
 }
 
