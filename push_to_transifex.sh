d57bf3184f9e609d60b1db64246f47436470ccda
Etienne Trimaille <gustrimaille@yahoo.fr>
2023-09-27 16:19:47 +0200

    Restore push to transifex when on a maintained branch

--- push_to_transifex.sh
+++ push_to_transifex.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export LOCALES_BRANCH=$(git branch | grep \* | cut -d ' ' -f2 | sed -r 's/_/-/g')
-
+export LOCALES_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+echo "Branch on transifex : "${LOCALES_BRANCH}
 tx push -s --branch $LOCALES_BRANCH
